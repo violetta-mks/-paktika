9f63ff2aec1f800f164c36e42b87929e1ce6fb80
violetta-mks <m.v.g20.04.2007@gmail.com>
2025-04-05 00:48:19 +0300

    Upload files

--- js/main.js
+++ js/main.js
@@ -124,11 +124,51 @@ var app = new Vue({
                 }
             }
         },
+        addToCart: function (id) {
+            if (window.localStorage.getItem('cart')) {
+                this.cart = window.localStorage.getItem('cart').split(',');
+            }
+
+            if (this.cart.indexOf(String(id)) == -1) {
+                this.cart.push(id);
+                window.localStorage.setItem('cart', this.cart.join());
+                this.btnVisible = 1;
+            }
+        },
         checkInCart: function () {
-            console.log("Checking if product is in cart");
+            if (this.product && this.product.id && window.localStorage.getItem('cart').split(',').indexOf(String(this.product.id)) != -1) this.btnVisible = 1;
+            if (window.localStorage.getItem('cart') !== null) this.cartVisible = 1;
         },
         getCart: function () {
-            console.log("Fetching cart data");
+            if (window.localStorage.getItem('cart')) {
+                this.cart = window.localStorage.getItem('cart').split(',');
+                for (var value of this.cart) {
+                    for (var index in this.products) {
+                        if (value == this.products[index].id) {
+                            this.product.push(this.products[index])
+                        }
+                    }
+                }
+            }
+        },
+        removeFromCart: function (id) {
+            for (var index in this.product) {
+                if (id == this.product[index].id) {
+                    this.product.splice(index, 1);
+                    this.cart.splice(index, 1)
+                }
+            }
+            window.localStorage.setItem('cart', this.cart.join(','));
+            this.getCart();
+            location.reload();
+        },
+        makeOrder: function () {
+
+            this.formVisible = 0;
+            this.cartVisible = 0;
+
+            this.cart = [];
+            window.localStorage.removeItem('cart');
         }
-    }
+    },
 });
